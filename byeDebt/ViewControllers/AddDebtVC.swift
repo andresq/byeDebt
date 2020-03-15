e7ab281b7ccad12e1d5c9dc405aee80a927ca3ca
Andres Quintero <hello@andresq.com>
2020-03-15 18:03:49 -0400

    add save button to AddDebtVC

--- byeDebt/ViewControllers/AddDebtVC.swift
+++ byeDebt/ViewControllers/AddDebtVC.swift
@@ -17,6 +17,8 @@ class AddDebtVC: UIViewController {
     
     let debtRateLabel = BDLabel()
     let debtRateTextField = BDTextField()
+    
+    let saveButton = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,6 +34,9 @@ class AddDebtVC: UIViewController {
         
         setupDebtRateLabel()
         setupDebtRateTextField()
+        
+        setupSaveButton()
+        dismissKeyboardTap()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -50,9 +55,9 @@ class AddDebtVC: UIViewController {
         debtNameLabel.backgroundColor = .systemBlue //DEBUGSTUFF
         debtNameLabel.text = "Debt Name"
         
-        // Top is attached to the view.topAnchor
+        // topAnchor is attached to the view.topAnchor
         NSLayoutConstraint.activate([
-            debtNameLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+            debtNameLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
             debtNameLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 50),
             debtNameLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -50),
             debtNameLabel.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.05)
@@ -63,9 +68,10 @@ class AddDebtVC: UIViewController {
         view.addSubview(debtNameTextField)
         
         debtNameTextField.placeholder = "Apple Card"
+        debtNameTextField.delegate = self
         debtNameTextField.backgroundColor = .systemPink //DEBUGSTUFF
         
-        // Top is attached to the debtNameLabel.topAnchor
+        // topAnchor is attached to the debtNameLabel.topAnchor
         NSLayoutConstraint.activate([
             debtNameTextField.topAnchor.constraint(equalTo: debtNameLabel.bottomAnchor),
             debtNameTextField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 50),
@@ -82,7 +88,7 @@ class AddDebtVC: UIViewController {
         debtBalanceLabel.text = "Debt balance"
         debtBalanceLabel.backgroundColor = .systemTeal //DEBUGSTUFF
         
-        // Top is attached to DebtTextField.bottomAnchor
+        // topAnchor is attached to DebtTextField.bottomAnchor
         NSLayoutConstraint.activate([
             debtBalanceLabel.topAnchor.constraint(equalTo: debtNameTextField.bottomAnchor, constant: 20),
             debtBalanceLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 50),
@@ -98,7 +104,7 @@ class AddDebtVC: UIViewController {
         debtBalanceTextField.keyboardType = .decimalPad
         debtBalanceTextField.backgroundColor = .systemRed //DEBUGSTUFF
         
-        // Top is attached to DebtBalanceLabel.bottomAnchor
+        // topAnchor is attached to DebtBalanceLabel.bottomAnchor
         NSLayoutConstraint.activate([
             debtBalanceTextField.topAnchor.constraint(equalTo: debtBalanceLabel.bottomAnchor),
             debtBalanceTextField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 50),
@@ -114,7 +120,7 @@ class AddDebtVC: UIViewController {
         debtRateLabel.text = "Debt Rate"
         debtRateLabel.backgroundColor = .systemIndigo // DEBUGSTUFF
         
-        //Attached to debtBalanceTextField.bottomAnchor
+        // topAnchor is attached to debtBalanceTextField.bottomAnchor
         NSLayoutConstraint.activate([
             debtRateLabel.topAnchor.constraint(equalTo: debtBalanceTextField.bottomAnchor, constant: 20),
             debtRateLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 50),
@@ -126,10 +132,11 @@ class AddDebtVC: UIViewController {
     func setupDebtRateTextField(){
         view.addSubview(debtRateTextField)
         
-        debtRateTextField.placeholder = "2.3"
+        debtRateTextField.placeholder = "2.30"
         debtRateTextField.keyboardType = .decimalPad
         debtRateTextField.backgroundColor = .systemOrange // DEBUGSTUFF
         
+        // topAnchor is attached to debtRateLabel.bottomAnchor
         NSLayoutConstraint.activate([
             debtRateTextField.topAnchor.constraint(equalTo: debtRateLabel.bottomAnchor),
             debtRateTextField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 50),
@@ -138,9 +145,65 @@ class AddDebtVC: UIViewController {
         ])
     }
     
+    func setupSaveButton(){
+        view.addSubview(saveButton)
+        
+        
+        saveButton.backgroundColor = .systemPink //DEBUGSTUFF
+        saveButton.setTitle("Save Debt", for: .normal)
+        saveButton.titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline) // Give a bold look
+        saveButton.layer.cornerRadius = 20
+        saveButton.addTarget(self, action: #selector(saveButtonTapped), for: .touchUpInside)
+        
+        
+        
+        saveButton.translatesAutoresizingMaskIntoConstraints = false
+        // bottomAnchor is attached to view.bottomAnchor
+        NSLayoutConstraint.activate([
+            saveButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -150),
+            saveButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 45),
+            saveButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -45),
+            saveButton.heightAnchor.constraint(equalToConstant: 75.00)
+        ])
+    }
+    
+    // Keyboard functions
+    func dismissKeyboardTap() {
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:)))
+        view.addGestureRecognizer(tap)
+    }
+    
+    
+    // Button Function
+    @objc func saveButtonTapped(){
+        print("You tapped the save button")
+        // TODO: Validate text field input
+        // TODO: Save the input as a Debt object
+        // TODO: pop back to the MainVC
+        
+        
+        let debtName = debtNameTextField.text ?? "no name"
+        let debtBalance = debtBalanceTextField.text ?? "00.00"
+        let debtRate = debtRateTextField.text ?? "0.0"
+        
+        let debtString = "\(debtName) has a \(debtBalance) at a rate of \(debtRate)"
+        print(debtString)
+        navigationController?.popViewController(animated: true)
+    }
+    
     
     
 
     
 
+}
+
+
+
+extension AddDebtVC: UITextFieldDelegate {
+    // What happens when you tap return key (in our case it says search)
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print("You are tapping the done key")
+        return true
+    }
 }
